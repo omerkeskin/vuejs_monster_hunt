d7aed5a8b263440036ea82f632dc5dca97878b3e
Ömer Keskin <omer.keskin@softtech.com.tr>
2020-05-03 13:52:41 +0300

    Final

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ new Vue({
         player_heal:100,
         monster_heal:100,
         game_is_on:false,
+        attack_multiple:10,
+        special_attack_multiple:25,
+        heal_up_multiple:20,
+        monster_attack_multiple:15,
+        log_text : {
+            attack : "OYUNCU ATAGI : ",
+            special_attack : "OZEL OYUNCU ATAGI : ",
+            monster_attack : "CANAVAR ATAGI : ",
+            heal_up : "ILKYARDIM : ",
+            give_up : "OYUNCU PES ETTI"
+        },
         logs:[]
     },
     methods : {
@@ -11,32 +22,32 @@ new Vue({
           this.game_is_on = true;
         },
         attack:function () {
-            var point = Math.ceil(Math.random() * 10);
+            var point = Math.ceil(Math.random() * this.attack_multiple);
             this.monster_heal -= point;
-            this.add_to_log({ turn : "p", text : "OYUNCU ATAGI ( "+point+" )"});
+            this.add_to_log({ turn : "p", text : this.log_text.attack + point});
             this.monster_attack();
         },
         special_attack:function () {
-            var point = Math.ceil(Math.random() * 25);
+            var point = Math.ceil(Math.random() * this.special_attack_multiple);
             this.monster_heal -= point;
-            this.add_to_log({ turn : "p", text : "OZEL OYUNCU ATAGI ( "+point+" )"});
+            this.add_to_log({ turn : "p", text : this.log_text.special_attack + point});
             this.monster_attack();
         },
         heal_up:function () {
-            var point = Math.ceil(Math.random() * 20);
+            var point = Math.ceil(Math.random() * this.heal_up_multiple);
             this.player_heal += point;
-            this.add_to_log({ turn : "p", text : "ILKYARDIM ( "+point+" )"});
+            this.add_to_log({ turn : "p", text : this.log_text.heal_up + point});
             this.monster_attack();
         },
         give_up:function () {
             this.player_heal = 0;
             this.game_is_on = false;
-            this.add_to_log({ turn : "p", text : "OYUNCU PES ETTI"});
+            this.add_to_log({ turn : "p", text : this.log_text.give_up});
         },
         monster_attack:function () {
-            var point = Math.ceil(Math.random() * 15);
+            var point = Math.ceil(Math.random() * this.monster_attack_multiple);
             this.player_heal -= point;
-            this.add_to_log({ turn : "m", text : "CANAVAR ATAGI ( "+point+" )"});
+            this.add_to_log({ turn : "m", text : this.log_text.monster_attack + point});
         },
         add_to_log : function (log) {
             this.logs.push(log);
@@ -64,6 +75,18 @@ new Vue({
                     this.monster_heal = 100;
                     this.logs = [];
                 }
+            }
+        }
+    },
+    computed : {
+        user_progress : function () {
+           return  {
+                width : this.player_heal + '%'
+            }
+        },
+        monster_progress : function () {
+            return  {
+                width : this.monster_heal + '%'
             }
         }
     }
